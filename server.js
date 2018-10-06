663e62442c25e8ad504879deb8364bb6d4ba18d8
Aly <38168385+alycapps@users.noreply.github.com>
2018-10-06 14:07:42 -0400

    switching setup for Mongoose

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
-// Dependencies
+// DEPENDENCIES
+//for setting up server
 var express = require("express");
-var mongojs = require("mongojs");
-
-// Require axios and cheerio. This makes the scraping possible
+//noSQl database
+var mongoose = require("mongoose");
+//will make automatic console.logs
+var logger = require("morgan");
+//for scraping website
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 //create express server
 var app = express();
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+//automatic logging
+app.use(logger("dev"));
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use static folder
+app.use(express.static("public"));
+
+// Require mongoose models
+var db = require("./models");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
+// Connect to Mongoose
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+//ROUTES
 
 
 
 
-// Listen on port 3000
+
+//start server listening on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
